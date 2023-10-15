a36b591980b85ae029a7cacbf6829fee22cff591
Behzad <behzad.fazelasl@gmail.com>
2023-10-15 19:57:25 +0200

    use executor to execute a task

--- src/main/java/org/behzadfz/Main.java
+++ src/main/java/org/behzadfz/Main.java
@@ -1,7 +1,26 @@
 package org.behzadfz;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.RejectedExecutionException;
+
+import org.behzadfz.concurrency.Invoker;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Executor executor = new Invoker();
+
+        try {
+            // executor invokes the submitted task instantly in the invoking thread (main thread)
+            executor.execute(()-> {
+                System.out.println(Thread.currentThread());
+            });
+        } catch (RejectedExecutionException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println(Thread.currentThread());
+
+        System.out.println("Terminate!");
     }
 }
