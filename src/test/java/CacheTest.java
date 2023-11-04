a7d177c7bf2ba989dd47965be9baf5481a38a7d8
Behzad <behzad.fazelasl@gmail.com>
2023-11-04 17:16:41 +0100

    test thread unsafe behaviour

--- src/test/java/CacheTest.java
+++ src/test/java/CacheTest.java
@@ -1,5 +1,6 @@
 
 import org.behzadfz.caching.CacheService;
+import org.behzadfz.caching.CounterService;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -33,5 +34,31 @@ public class CacheTest {
         cacheService.forget("foo");
 
         assertEquals(cacheService.get("foo"), null);
+    }
+
+    @Test
+    public void testThreadUnsafeCache() throws Exception {
+        CounterService counter = new CounterService();
+
+        Thread a = new Thread(() -> {
+            for (int i = 0;i < 5; i++) {
+                counter.increase();
+            }
+        });
+
+        Thread b = new Thread(() -> {
+            for (int i = 0;i < 5; i++) {
+                counter.increase();
+            }
+        });
+
+        a.start();
+        b.start();
+
+        a.join();
+        b.join();
+
+        // counter should be 10 in a single thread app but not guarantee in multi thread app
+        assertNotEquals(counter.getCounter(), 10);
     }
 }
