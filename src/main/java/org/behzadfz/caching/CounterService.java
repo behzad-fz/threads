1167096e63a8f1745df355c6a2bc9b195877523a
Behzad <behzad.fazelasl@gmail.com>
2023-11-04 17:16:14 +0100

    add counter service

---
+++ src/main/java/org/behzadfz/caching/CounterService.java
@@ -0,0 +1,22 @@
+package org.behzadfz.caching;
+
+public class CounterService {
+    private final CacheService cacheService;
+
+    public CounterService() {
+        this.cacheService = new CacheService();
+    }
+
+    public void increase() {
+        Integer value = (Integer) cacheService.get("counter");
+
+        if (value == null) {
+            value = 0;
+        }
+        cacheService.put("counter", value + 1);
+    }
+
+    public int getCounter() {
+        return (int) cacheService.get("counter");
+    }
+}
