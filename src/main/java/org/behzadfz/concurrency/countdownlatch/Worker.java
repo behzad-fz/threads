fc10de230d9ea93ebd2a14570b39d90497b8b942
Behzad <behzad.fazelasl@gmail.com>
2023-11-22 23:03:12 +0100

    add a worker to count down

---
+++ src/main/java/org/behzadfz/concurrency/countdownlatch/Worker.java
@@ -0,0 +1,20 @@
+package org.behzadfz.concurrency.countdownlatch;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+public class Worker implements Runnable{
+    private List<String> outputScraper;
+    private CountDownLatch countDownLatch;
+
+    public Worker(List<String> outputScraper, CountDownLatch countDownLatch) {
+        this.outputScraper = outputScraper;
+        this.countDownLatch = countDownLatch;
+    }
+    @Override
+    public void run() {
+        System.out.println("do something!");
+        outputScraper.add("Counted down");
+        countDownLatch.countDown();
+    }
+}
